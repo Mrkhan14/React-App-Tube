3e9c9cb88dcdb22a9beb64f8d0bbfce0d4567393
Mrkhan14 <aabdulaxadkhan@gmail.com>
2024-04-05 17:32:22 +0500

    File structure & Material UI

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import {Stack, Button } from '@mui/material';
 
 const App = () => {
   return (
-    <div>
-      Appss
-    </div>
+   <Stack spacing={2} direction="row">
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </Stack>
   )
 }
 
